d92b5c07467bafe17a060a6eba7ee18f4340dc25
Sacha Perry-Fagant <sacha.perry-fagant@mail.mcgill.ca>
2018-06-11 01:09:51 -0400

    Change ontario scraper to work with new website

--- Lexamind/Scraper/scrapers/ontario_scraper.py
+++ Lexamind/Scraper/scrapers/ontario_scraper.py
@@ -5,18 +5,23 @@ Created on Thu Jan  4 15:45:54 2018
 @author: Sacha Perry-Fagant
 """
 
+# Make sure we can access the resoures file
+import sys
+import os
+parentDir = os.getcwd().split('\\Scraper')[0]
+if parentDir not in sys.path:
+    sys.path.insert(0,parentDir)
+
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
 import requests
-from .scraper_api import Scraper, Bill
+from scraper_api import Scraper, Bill
 import csv
 import re
 import locale, time
 from datetime import datetime
 
 
-# the main function that scrapes the Legislative Assembly of Ontario
-# it will be saved as Legislative_Assembly_of_Ontario.csv
 class Ontario( Scraper ):
 
     rgx_modified_title = '.*Loi modifiant.*'
@@ -26,9 +31,11 @@ class Ontario( Scraper ):
         super(Scraper, self).__init__()
         self.legislature="Ontario"
 
-    def retrieve_bills(self,  filename = None):
-        url = "http://www.ontla.on.ca/web/bills/bills_all.do?locale=fr"
-        url_base = "http://www.ontla.on.ca/web/bills/"
+    # If we want only the scrape the most recent bills
+    # Set most_recent to True
+    def retrieve_bills(self, most_recent = False, filename = None):
+        url = "https://www.ola.org/fr/affaires-legislatives/projets-loi/"
+        url_base = "https://www.ola.org"        
 
         # Where all the data from the bills will be stored
         data = []
@@ -46,105 +53,121 @@ class Ontario( Scraper ):
 
         page = urllib2.urlopen(url)
         soup = BeautifulSoup(page, "html.parser")
-        bills = soup.find_all("td", attrs = {"class": "nocell"})
+        
+        soup = soup.find('table')
+        sessions = soup.findAll('tr')[1:]
+        
+        if most_recent:
+            sessions = [sessions[0]]
+        
+        for session in sessions:
+            row = session.find('td')
+            if not row:
+                continue
+            session = row.text
+            session_url = url_base + row.find('a')['href']
 
-        counter = 1
-        for bill in bills:
-            if counter % 10 == 0:
-                print("Currently at bill " + str(counter))
-            counter += 1
+            page = urllib2.urlopen(session_url)
+            soup = BeautifulSoup(page, "html.parser")
+            soup = soup.find('tbody')
+            bills = soup.find_all("tr")
 
-            # Data is separated by commas in the CSV so the commas are removed
-            bill_info = {}
-            # the id is bill73 for example
-            bill_info['id'] = bill.text
-
-            # has the title and the url for the detailed data
-            title_url = bill.findNext('td',attrs = {'class': 'titlecell'})
-
-            # the id is bill73 for example
-            identifier = self.legislature + bill.text
-
-            title = title_url.text.strip().replace(","," ")
-
-            billInst = Bill(identifier, title, self.legislature)
-
-            bill_info['title'] = title_url.text.strip().replace(","," ")
-            bill_info['sponsor'] = bill.findNext('td',attrs = {'class': 'sponsorcell'}).text
-
-            bill_info['date'] = []
-            bill_info['stage'] = []
-            bill_info['activity'] = []
-            bill_info['committee'] = []
-
-            # Getting the url of the detailed info
-            info_url = url_base + title_url.find('a')['href']
-            bill_info = Ontario.Extract_Events_Ontario(info_url, bill_info, billInst)
-            bill_info['details'] = Ontario.Extract_Info_Ontario(info_url)
-            billInst.setDetails(Ontario.Extract_Info_Ontario(info_url))
-
-            self.scrapeLawsinBill(billInst)
-
-            data.append(billInst)
+            counter = 1
+            for bill in bills:
+                bill = bill.findNext('td')
+                bill_info = {}
+        
+                bill_info['id'] = bill.text.strip()
+                
+                if counter % 10 == 0:
+                    print("Currently at bill " + bill_info['id'])
+                    return data
+                counter += 1
+                
+                # has the title and the url for the detailed data
+                title_url = bill.findNext('a')
+                
+                # the id is bill73 for example
+                identifier = self.legislature + bill.text
+                
+                title = title_url.text.strip()
+                
+                billInst = Bill(identifier, title, self.legislature)
+                
+                bill_info['title'] = title
+    
+                bill_info['sponsor'] = bill.findNext('td', attrs = {'headers': 'view-field-member-table-column'}).text.strip()
+                
+                bill_info['date'] = []
+                bill_info['stage'] = []
+                bill_info['activity'] = []
+                bill_info['committee'] = []
+                # Getting the url of the detailed info
+        
+                info_url = url_base + title_url['href']
+                bill_info = Ontario.Extract_Events_Ontario(info_url, bill_info, billInst)
+                bill_info['details'] = Ontario.Extract_Info_Ontario(info_url)                
+                bill_info['url'] = info_url
+                
+                billInst.setDetails(Ontario.Extract_Info_Ontario(info_url))
+    
+                self.scrapeLawsinBill(billInst)
+    
+                data.append(billInst)
 
         if filename == None:
-            # it will be saved as Legislative_Assembly_of_Ontario.csv
-            filename = soup.find("meta", attrs = {'name' : "dc:publisher"})['content']
-            filename = filename.replace(" ", "_")
-            filename += ".csv"
+            filename = 'Legislative_Assembly_of_Ontario.csv'
 
         self.bills=data
         return data
 
     def Extract_Events_Ontario(url, bill_info, billInst):
-
         data = {}
-
+        
         # get the details of events
-        url = url.replace('ParlSessionID=','&detailPage=bills_detail_status')
-
+        url = url + '/etapes'
+        
         try:
             response = requests.get(url)
         except:
             print("There was an issue connecting to the internet")
             return
-
+        
         if response.status_code != 200:
             print("There was an error finding the detailed page")
             return
-
+        
         page = urllib2.urlopen(url)
         soup = BeautifulSoup(page).find('table')
-
+        
         if not soup:
             time.sleep(1)
             # time out to let reconnect to internet
             soup = soup = BeautifulSoup(page, "html.parser").find('table')
             if not soup:
                 return bill_info
-
-        counter = 0
-        data = soup.find('tr', attrs = {'class':'evenrow'})
+        
+        soup = soup.find('tr')
+        data = soup.findNext('tr')
         while data:
-
-            date=data.findNext('td', attrs = {'class' : "date"}).text.strip().replace(","," ")
-            stage=data.findNext('td', attrs = {'class' : "stage"}).text.strip().replace(","," ")
-            activity=data.findNext('td', attrs = {'class' : "activity"}).text.strip().replace(","," ")
-            committee=data.findNext('td', attrs = {'class' : "committee"}).text.strip().replace(","," ")
+            col = data.findNext('td')
+            date = Ontario.get_date(col.text.strip())
             bill_info['date'].append(date)
+            col = col.findNext('td')
+            stage = col.text.strip()
             bill_info['stage'].append(stage)
+            col = col.findNext('td')
+            activity = col.text.strip()
             bill_info['activity'].append(activity)
+            col = col.findNext('td')
+            committee = col.text.strip()
             bill_info['committee'].append(committee)
+            
             billInst.addEvent(stage, date, activity, committee)
-
-            counter += 1
-            if counter % 2 == 0:
-                data = data.findNext('tr', attrs = {'class':'evenrow'})
-            else:
-                data = data.findNext('tr', attrs = {'class':'oddrow'})
-
-
+            data = data.findNext('tr')
+        
         return bill_info
+
 
 
     # Gets all the text from the detailed info page
@@ -207,3 +230,47 @@ class Ontario( Scraper ):
             except:
                 pass
         locale.setlocale(locale.LC_TIME, "en-CA")
+
+    # Get date, returns ##/##/#### - day/month/year
+    def get_date(date):
+        date = date.lower()
+        # Finding the numbers in the date
+        # The first is the date, the second is the year
+        
+        day = date.split(' ')[0]
+        year = date.split(' ')[2]
+        
+        if int(day) < 10:
+            day = '0' + day
+    
+        month = ""
+        
+        if 'jan' in date:
+            month = '01'
+        elif 'feb' in date:
+            month = '02'
+        elif 'mar' in date:
+            month = '03'
+        elif 'avr' in date or 'apr' in date:
+            month = '04'
+        elif 'mai' in date or 'may' in date:
+            month = '05'
+        elif 'juin' in date or 'jun' in date:
+            month = '06'
+        elif 'juil' in date or 'jul' in date:
+            month = '07'
+        elif 'ao' in date or 'aug' in date:
+            month = '08'
+        elif 'sep' in date:
+            month = '09'
+        elif 'oct' in date:
+            month = '10'
+        elif 'nov' in date:
+            month = '11'
+        elif 'dec' in date:
+            month = '12'
+        else:
+            # No month
+            return False
+        
+        return day + '/' + month + '/' + year
