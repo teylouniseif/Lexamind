088ef3d21d1fa47102babfd21d0954029d696382
Sacha Perry-Fagant <sacha.perry-fagant@mail.mcgill.ca>
2018-07-12 21:50:53 -0400

    Extract text with xpdfreader from https://www.xpdfreader.com/

--- Lexamind/Scraper/scrapers/gazette_quebec_scraper.py
+++ Lexamind/Scraper/scrapers/gazette_quebec_scraper.py
@@ -4,6 +4,13 @@ Created on Sat Mar  3 15:28:38 2018
 @author: Sacha Perry-Fagant
 """
 
+# Make sure we can access the resoures file
+import sys
+import os
+parentDir = os.getcwd().split('\\Scraper')[0]
+if parentDir not in sys.path:
+    sys.path.insert(0,parentDir)
+
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
 import requests
@@ -15,6 +22,7 @@ import csv
 
 encrypted_file = "encr.pdf"
 decrypted_file = "decry.pdf"
+text_file = "text.txt"
 
 """
 info to get:
@@ -23,7 +31,7 @@ gazette date
 
 for each law of interest, any sort of action that is tied to it, reglement, decret, decisions
 """
-from .scraper_api import Scraper, Bill
+from scraper_api import Scraper, Bill
 
 class GazetteQuebec( Scraper ):
 
@@ -161,10 +169,16 @@ class GazetteQuebec( Scraper ):
 
         if pdfFile.isEncrypted:
             fp, pdfFile = GazetteQuebec.decrypt_pdf(url)
-            for pageNum in range(pdfFile.getNumPages()):
-                currentPage = pdfFile.getPage(pageNum)
-                data += currentPage.extractText()
             fp.close()
+            
+            # Convert pdf to text
+            os.system("pdftotext " + decrypted_file + " " + text_file)
+            
+            with open(text_file, encoding="utf8") as fl:
+                for line in fl:
+                    data += line
+                fl.close()
+            
             # delete the files so we can reuse them
             os.system("del " + encrypted_file)
             os.system("del " + decrypted_file)
@@ -173,6 +187,9 @@ class GazetteQuebec( Scraper ):
             for pageNum in range(pdfFile.getNumPages()):
                 currentPage = pdfFile.getPage(pageNum)
                 data += currentPage.extractText()
+            
+            os.system("del " + text_file)
+
 
         return data
 
